34b343c2b41bef959f549dc5a8b87b9fbf2bf0b1
asl <asl@opencascade.com>
2017-09-29 14:11:41 +0300

    unicode support

--- datamodel/_all_tests_.py
+++ datamodel/_all_tests_.py
@@ -3,8 +3,10 @@ import unittest
 
 from test_model import TestModel
 from test_generators import TestGenerators
-
+from unicode_utils import init
+ 
 try:
+    init()
     unittest.main()
 except Exception as error:
     print "Unexpected error:", format(error)
